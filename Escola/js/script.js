0999bfb2a5488e83c63461f3d0a9a6c4f2b10402
Isaa <134286004+Belzs10@users.noreply.github.com>
2024-04-03 10:15:33 -0300

    Add files via upload

---
+++ Escola/js/script.js
@@ -0,0 +1,13 @@
+//dom
+
+const nome = document.querySelector('#nome')
+const uni1 = document.querySelector('#uni1')
+const uni2 = document.querySelector('#uni2')
+const uni3 = document.querySelector('#uni3')
+const btn = document.querySelector('#btm')
+const resultado =  document.querySelector('#resultado')
+
+//evento
+
+btn.addEventListener('click',()=>{
+})
